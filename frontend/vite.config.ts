80287dfd2438bd6e703730581d9fd70ec78cc64a
alex <alexjilson007@gmail.com>
2026-07-19 19:59:17 +0530

    Revert vite base path for Vercel/Onslate deployments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
   envDir: path.resolve(__dirname, '..'),
   envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
